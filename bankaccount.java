5b3c3ebd9f92fa9bd3f81789092f684b95c97f28
Asiedu Epaphras Kwabena <ekasiedu017@st.ug.edu.gh>
2026-02-06 08:02:02 +0000

    I have uploaded  code on the class Bank Account in Java to explain the concept of encapsulation

---
+++ bankaccount.java
@@ -0,0 +1,12 @@
+class BankAccount { 
+    private double balance;
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount
+        }
+    }
+    public double getBalance() {
+        return balance;
+    }
+
+}
