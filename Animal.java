0ee63159dbe72be8a14206dc6efac7998932c402
Asiedu Epaphras Kwabena <ekasiedu017@st.ug.edu.gh>
2026-02-06 08:21:27 +0000

    Uploading code using the concept of inheritance

---
+++ Animal.java
@@ -0,0 +1,11 @@
+class Animal {
+    void eat() {
+        System.out.println("This animal is eating");
+    }
+}
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Woof")
+    }`
+
+} 
